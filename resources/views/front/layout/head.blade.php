c8cd6d1b813844b0b2652ac7613684ffa2e8b1f4
andiii24 <andiialpha@gmail.com>
2023-06-09 15:45:21 +0300

    First push

---
+++ resources/views/front/layout/head.blade.php
@@ -0,0 +1,63 @@
+<head>
+    <meta charset="UTF-8">
+    <meta
+        http-equiv="X-UA-Compatible"
+        content="IE=edge"
+    >
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+    >
+    <!-- favicon -->
+    <link
+        rel="shortcut icon"
+        type="image/x-icon"
+        href="{{ asset('assets/images/fav.svg') }}"
+    >
+    <title> Kertina Bar & Restaurant </title>
+
+    <!-- fontawesome css -->
+    <link
+        rel="stylesheet"
+        href="{{ asset('assets/css/plugins/fontawesome-5.css') }}"
+    >
+    <!-- fontawesome css -->
+    <link
+        rel="stylesheet"
+        href="{{ asset('assets/css/plugins/swiper.css') }}"
+    >
+    <link
+        rel="stylesheet"
+        href="{{ asset('assets/css/plugins/unicons.css') }}"
+    >
+    <link
+        rel="stylesheet"
+        href="{{ asset('assets/css/plugins/metismenu.css') }}"
+    >
+    <link
+        rel="stylesheet"
+        href="{{ asset('assets/css/plugins/magnific-popup.css') }}"
+    >
+    <link
+        rel="stylesheet"
+        href="{{ asset('assets/css/plugins/timepickers.min.css') }}"
+    >
+    <link
+        rel="stylesheet"
+        href="{{ asset('assets/css/plugins/aos.css') }}"
+    >
+    <link
+        rel="stylesheet"
+        href="{{ asset('assets/css/plugins/hover-revel.css') }}"
+    >
+    <!-- bootstrap css -->
+    <link
+        rel="stylesheet"
+        href="{{ asset('assets/css/vendor/bootstrap.min.css') }}"
+    >
+    <!-- main css -->
+    <link
+        rel="stylesheet"
+        href="{{ asset('assets/css/style.css') }}"
+    >
+</head>
